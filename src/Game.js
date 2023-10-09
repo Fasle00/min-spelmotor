35a0e5f401531ff6091c9a5a861164175c9e3cd9
Fabian Sigfridsson <69erik355@gmail.com>
2023-10-09 13:38:10 +0200

    ✨ introduced player and inputhandler

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,28 @@
+import InputHandler from "./Inputhandler"
+import Player from "./Player"
+
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
+    this.input = new InputHandler(this)
     this.keys = []
     this.enemies = []
     this.gameOver = false
     this.gravity = 1
     this.debug = false
+
+    this.player = new Player(this)
   }
 
   update(deltaTime) {
     if (!this.gameOver) {
       this.gameTime += deltaTime
     }
+    this.player.update(deltaTime)
   }
 
-  draw(context) {}
+  draw(context) {
+    this.player.draw(context)
+  }
 }
