1708b098d24bda52ba68fccf4b03230583ea49c4
Fabian Sigfridsson <69erik355@gmail.com>
2023-10-10 10:30:45 +0200

    ✨ Enemys

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,7 @@
 import InputHandler from "./Inputhandler"
 import Player from "./Player"
 import UserInterface from "./UserInterface"
+import Pumpkin from "./Pumpkin";
 
 export default class Game {
   constructor(width, height) {
@@ -9,14 +10,17 @@ export default class Game {
     this.input = new InputHandler(this)
     this.ui = new UserInterface(this)
     this.keys = []
-    this.enemies = []
+    
     this.gameOver = false
     this.gameTime = 0
     this.gravity = 1
     this.debug = false
 
-    this.ammo = 10
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
 
+    this.ammo = 10
     this.player = new Player(this)
   }
 
@@ -26,13 +30,32 @@ export default class Game {
     }
     this.gameTime += deltaTime
     this.player.update(deltaTime)
+
+    if (this.enemyTimer > this.enemyInterval) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deltaTime
+    }
+
+    this.enemies.forEach((enemy) => enemy.update(deltaTime))
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
   }
 
   draw(context) {
+    this.displayAmmo(context)
+    this.player.draw(context)
+    this.ui.draw(context)
+    this.enemies.forEach(enemy => enemy.draw(context))
+  }
+
+  displayAmmo(context){
     context.fillStyle = '#000'
     context.font = '20px serif'
     context.fillText(`Ammo: ${this.ammo}`, 10, 20)
-    this.player.draw(context)
-    this.ui.draw(context)
+  }
+
+  addEnemy() {
+    this.enemies.push(new Pumpkin(this))
   }
 }
